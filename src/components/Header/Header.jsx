c23da90a5da16fc2c7ca8d04b9542070e5d0171a
YashPK <pratapyashkulshrestha@gmail.com>
2024-07-06 19:19:10 +0530

    chore: Add Button and Input components

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -42,7 +42,31 @@ function Header() {
 
 
   return (
-    <div className=''>Header</div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+            <div className='mr-4'>
+              <Link to='/'>
+                <Logo width='70px' />              
+              </Link>
+            </div>
+            <ul className='flex ml-auto'>
+                {navItems.map((item) => 
+                  item.active ? (
+                    <li key={item.name}>
+                      <button onClick={() => navigate(item.slug)}  className='inline-block px-6 py-2 duration-200 hover:bg-blue-200 rounded-full'>{item.name}</button>
+                    </li>
+                  ) : null                
+                )} 
+                {authStatus && (
+                  <li>
+                    <LogoutBtn />
+                  </li>
+                )} 
+            </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
