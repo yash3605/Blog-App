0d46a09403402f7375fd47e88f84e59a16a268fe
YashPK <pratapyashkulshrestha@gmail.com>
2024-07-26 21:16:24 +0530

    chore: Update Login component with styling and error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,8 +13,56 @@ function Login() {
     const dispatch = useDispatch()
     const { register, handleSubmit } = useForm()
     const [error, setError] = useState('')
+
+    const login = async (data) => {
+       setError('')
+       try {
+        const session = await authService.login(data)
+         if(session) {
+           const userData = await authService.getCurrentUser()
+           if(userData) {
+            dispatch(authLogin(userData))
+           }
+           navigate('/')
+         }
+       } catch (error) {
+           setError(error.message)
+       }
+    }
   return (
-    <div>Login</div>
+    <div className='flex items-center justify-center w-full'>
+      <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+          <div className='mb-2 flex justify-center'> 
+              <span className='inline-block w-full max-w-[100px]'>
+                LOGO
+              </span>
+          </div>
+          <h2 className='text-center text-2xl font-bold leading-tight'> Sign in to your account</h2>
+          <p className='mt-2 text-center text-base text-black/60'>
+               Don&apos;t have an account? &nbsp;
+               <Link
+                 to="/signup"
+                 className='font-medium text-primary transition-all duration-200 hover:underline'
+               >Sign Up</Link>
+          </p>
+          {error && <p className='text-red-500 text-center mt-8'>{error}</p>}
+          <form onSubmit={handleSubmit(login)} className='mt-8'>
+              <div className='space-y-5'>
+                  <Input
+                    label= 'Email:'
+                    placeholder='Enter your email'
+                    type='email'
+                    {...register('email', {
+                       required: true,
+                       validate: {
+                         matchPattern: (value) => /^([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}$/igm .test(value) || 'Invalid email address'
+                       }
+                      })}
+                 />
+              </div>
+          </form>
+      </div>
+    </div>
   )
 }
 
